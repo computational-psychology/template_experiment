dea19bdb2c019edee0970e7b4b9c55cb9030f43c
Joris Vincent <joris.vincent@tu-berlin.de>
2023-07-06 15:32:00 +0200

    refactor: simplify show_stimulus

--- matching_experiment/stimuli.py
+++ matching_experiment/stimuli.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 
-def show_stimulus(ihrl, stimulus_texture, matching_field_img, match_intensity):
-    # draw the checkerboard
+def show_stimulus(
+    ihrl,
+    stimulus_texture,
+    matching_field_stim,
+    match_intensity,
+):
+    # Draw the stimulus
     stimulus_texture.draw(
         (
             (ihrl.width // 2) - stimulus_texture.wdth / 2,
@@ -10,22 +15,14 @@ def show_stimulus(ihrl, stimulus_texture, matching_field_img, match_intensity):
         )
     )
 
-    # create matching field with adjusted luminance
-    matching_display = show_match(ihrl, match_intensity, matching_field_img)
-    matching_display.draw(
-        ((ihrl.width // 2) - matching_display.wdth / 2, ((ihrl.height // 2)) / 4 - 50)
+    # Update matching field intensity
+    matching_field_stim["img"] = np.where(matching_field_stim["field_mask"], match_intensity, matching_field_stim["img"])
+
+    # Draw the matching field
+    matching_texture = ihrl.graphics.newTexture(matching_field_stim["img"])
+    matching_texture.draw(
+        ((ihrl.width // 2) - matching_texture.wdth / 2, ((ihrl.height // 2)) / 4 - 50)
     )
 
     # flip everything
     ihrl.graphics.flip(clr=False)  # clr= True to clear buffer
-
-
-def show_match(ihrl, match_intensity, matching_field_img):
-    # replace the center patch on top of the match_display
-    # and adjust it to the matched luminace
-
-    center = np.copy(matching_field_img)
-    center[center < 0] = match_intensity
-    # create new texture
-    center_display = ihrl.graphics.newTexture(center)
-    return center_display
