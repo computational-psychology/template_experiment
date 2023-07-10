19e7aa97f1dc2292bc6740ed5494969a070d5dbc
Joris Vincent <joris.vincent@tu-berlin.de>
2023-07-10 18:33:03 +0200

    refactor: bring run_experiment in line with others

--- matching_experiment/experiment/run_experiment.py
+++ matching_experiment/experiment/run_experiment.py
@@ -18,46 +18,39 @@ import pandas as pd
 import text_displays
 from hrl import HRL
 
-inlab_siemens = "vlab" in gethostname()
-inlab_viewpixx = "viewpixx" in gethostname()
-
-if inlab_siemens:
+if "vlab" in gethostname():
     SETUP = {
         "graphics": "datapixx",
         "inputs": "responsepixx",
         "scrn": 1,
         "lut": "lut.csv",
         "fs": True,
+        "wdth": 1024,
+        "hght": 768,
+        "bg_blank": 0.1,  # corresponding to 50 cd/m2 approx
     }
-    # size of Siemens monitor
-    WIDTH = 1024
-    HEIGHT = 768
-    bg_blank = 0.1  # corresponding to 50 cd/m2 approx
-
-elif inlab_viewpixx:
+elif "viewpixx" in gethostname():
     SETUP = {
         "graphics": "viewpixx",
         "inputs": "responsepixx",
         "scrn": 1,
         "lut": "lut_viewpixx.csv",
         "fs": True,
+        "wdth": 1920,
+        "hght": 1080,
+        "bg_blank": 0.27,  # corresponding to 50 cd/m2 approx
     }
-    # size of VPixx monitor
-    WIDTH = 1920
-    HEIGHT = 1080
-    bg_blank = 0.27  # corresponding to 50 cd/m2 approx
-
 else:
     SETUP = {
         "graphics": "gpu",
         "inputs": "keyboard",
         "scrn": 0,
         "lut": None,
-        "fs": True,
+        "fs": False,
+        "wdth": 1024,
+        "hght": 768,
+        "bg_blank": 0.3,
     }
-    WIDTH = 1024
-    HEIGHT = 768
-    bg_blank = 0.27
 
 
 def run_block(ihrl, block, block_id):
@@ -83,6 +76,10 @@ def run_block(ihrl, block, block_id):
         trial_results = experiment_logic.run_trial(ihrl, **trial)
         trial.update(trial_results)
         t2 = pd.Timestamp.now().strftime("%Y%m%d:%H%M%S.%f")
+
+        # Record timing
+        trial["start_time"] = t1
+        trial["stop_time"] = t2
 
         # Save trial
         data_management.save_trial(trial, block_id)
@@ -122,14 +119,9 @@ def experiment_main(ihrl):
 
 
 if __name__ == "__main__":
-    LANG = "en"  # 'de' or 'en'
-
     # Create HRL interface object with parameters that depend on the setup
     ihrl = HRL(
         **SETUP,
-        wdth=WIDTH,
-        hght=HEIGHT,
-        bg=bg_blank,
         photometer=None,
         db=True,
     )
